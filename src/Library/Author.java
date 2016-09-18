b181358cc5c272ab8da5b3725090011be767415e
SealHigh <martin.renstrom1@gmail.com>
2016-09-18 15:31:13 +0200

    fixed book and author

--- src/Library/Author.java
+++ src/Library/Author.java
@@ -4,4 +4,14 @@ package Library;
  * Created by Martin on 2016-09-18.
  */
 public class Author {
+
+    private String name;
+
+    public Author(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/Library/Book.java
+++ src/Library/Book.java
@@ -1,7 +1,28 @@
 package Library;
 
+import java.util.ArrayList;
+
 /**
  * Created by Martin on 2016-09-18.
  */
 public class Book {
+
+    private String isbn;
+    private String title;
+    private int edition;
+    private double price;
+    private ArrayList<Author> authors;
+
+    public Book(){
+        authors = new ArrayList<>();
+    }
+
+    public void addAuthor(String name){
+        authors.add(new Author(name));
+    }
+
+    public ArrayList getAuthors(){
+        return authors;
+    }
+
 }
