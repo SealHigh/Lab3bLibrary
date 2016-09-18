8120c5521d9517b6aa85b81a9b0123b3f9d6c287
timothy <timhol@kth.se>
2016-09-18 15:30:13 +0200

    Skeleton

--- src/Library/CollectionOfBooks.java
+++ src/Library/CollectionOfBooks.java
@@ -1,7 +1,16 @@
 package Library;
 
+import java.util.ArrayList;
+
 /**
  * Created by Martin on 2016-09-18.
  */
 public class CollectionOfBooks {
+    public void addBook(Book book) {
+
+    }
+
+    public ArrayList<Book> getBooksByTitle(String title) {
+        return null;
+    }
 }
