c4b962e885a14b8bd94109191731a502ec6ef09b
timothy <timhol@kth.se>
2016-09-27 21:09:45 +0200

    JavaDoc

--- src/Library/Author.java
+++ src/Library/Author.java
@@ -14,14 +14,27 @@ public class Author implements Serializable {
 
     private String name;
 
+    /**
+     * Initializing a new Author with a set name
+     *
+     * @param name Name of the author
+     */
     public Author(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the author's name
+     *
+     * @return Author's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return Author's name
+     */
     @Override
     public String toString() {
         return name;
