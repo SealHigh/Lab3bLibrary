dfbcc6473f9d66d41757f88d715f398119eb2b0b
timothy <timhol@kth.se>
2016-09-27 13:53:12 +0200

    Typo

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -23,7 +23,6 @@ public class UserInterface {
     private BooksFileManager BFM;
     private String fileName = "Library/library.ser";
 
-
     public UserInterface() {
         library = new CollectionOfBooks();
         scanner = new Scanner(System.in);
@@ -167,7 +166,7 @@ public class UserInterface {
             scanner.nextLine();
             System.out.println("Double, please!");
         }
-        double input = scanner.nextInt();
+        double input = scanner.nextDouble();
         scanner.nextLine();
         return input;
     }
